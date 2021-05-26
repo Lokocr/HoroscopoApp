3ae0dc6f2bdb63ac5825a5460c9cecd2b40d1b22
Jose Cartin <jacarve@gmail.com>
2021-05-25 22:30:15 -0600

    Se implementa Admob

--- app/flutter_horoscopo/lib/helpers/adsHelper.dart
+++ app/flutter_horoscopo/lib/helpers/adsHelper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/6300978111";
+      return "ca-app-pub-5452229565855763/2639790728";
     }
     // Proximamente iOS.
     else {
@@ -11,13 +11,13 @@ class AdHelper {
     }
   }
 
-  static String get nativeAdUnitId {
-    if (Platform.isAndroid) {
-      return "ca-app-pub-3940256099942544/2247696110";
-    }
-    // Proximamente iOS.
-    else {
-      throw new UnsupportedError("Unsuported platform");
-    }
-  }
+  // static String get nativeAdUnitId {
+  //   if (Platform.isAndroid) {
+  //     return "ca-app-pub-3940256099942544/2247696110";
+  //   }
+  //   // Proximamente iOS.
+  //   else {
+  //     throw new UnsupportedError("Unsuported platform");
+  //   }
+  // }
 }
